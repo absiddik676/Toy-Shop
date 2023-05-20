464edbfb6dfb7841a99475b50fc60ac94877331e
absiddik676 <absiddik8200@gmail.com>
2023-05-20 15:17:53 +0600

    when click  update button  open a modal added

--- src/page/MyToy/MyToy.jsx
+++ src/page/MyToy/MyToy.jsx
@@ -4,7 +4,8 @@ import MyToyRow from './MyToyRow';
 
 const MyToy = () => {
     const { user } = useContext(AuthContext);
-    const [myToys,setMyToys] = useState([])
+    const [myToys,setMyToys] = useState([]);
+    const [updatededToyinfo,setUpdatededToyinfo] = useState({})
 
     useEffect(() => {
         fetch(`http://localhost:5000/myToy/${user?.email}`)
@@ -12,7 +13,10 @@ const MyToy = () => {
             .then(data => setMyToys(data))
     }, [])
 
-
+    const handelOpenModal = (toyInfo) =>{
+        setUpdatededToyinfo(toyInfo);
+    }
+console.log(updatededToyinfo);
     return (
         <div>
             <div className="overflow-x-auto max-w-7xl mx-auto">
@@ -29,12 +33,70 @@ const MyToy = () => {
                     </thead>
                     <tbody>
                         {
-                            myToys.map((toys,index) =><MyToyRow index={index} key={toys._id} toys={toys}></MyToyRow>)
+                            myToys.map((toys,index) =><MyToyRow
+                             index={index} 
+                             key={toys._id} 
+                             toys={toys}
+                             handelOpenModal={handelOpenModal}
+                             ></MyToyRow>)
                         }
                        
                        
                     </tbody>
                 </table>
+            </div>
+
+            <div className="modal" id="my-modal-2">
+                <div className="modal-box">
+                    <h3 className="font-bold text-lg text-center mb-6"> Update <span className='text-pink-500'>{'ToyName'}</span> Information </h3>
+                    <form action="">
+                    <div className='flex gap-5 '>
+                        <div className="mb-4 w-1/2">
+                            <label className="block  text-black font-bold mb-2" htmlFor="name">
+                                Toy Price
+                            </label>
+                            <input
+                               
+                                className="w-full  focus:outline-none focus:ring-2 focus:ring-indigo-500 border border-gray-300 p-2 rounded-md"
+                                type="text"
+                                id="Price"
+                                name="Price"
+                                defaultValue={'price'}
+                            />
+                        </div>
+                        <div className="mb-4 w-1/2">
+                            <label className="block text-black font-bold mb-2" htmlFor="pictureUrl">
+                            quantity
+                            </label>
+                            <input
+                                
+                                className="w-full focus:outline-none focus:ring-2 focus:ring-indigo-500 border border-gray-300 p-2 rounded-md"
+                                type="text"
+                                id="quantity"
+                                name="quantity"
+                                defaultValue={'quantity'}
+                            />
+                        </div>
+                    </div>
+                    <div className="mb-4">
+                        <label className="block text-black font-bold mb-2" htmlFor="description">
+                            Detail Description
+                        </label>
+                        <textarea
+                            
+                            className="w-full border border-gray-300 p-2 rounded-md resize-none h-20 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                            id="description"
+                            name="description"
+                            rows="4"
+                            defaultValue={'description'}
+                        ></textarea>
+                    </div>
+                    </form>
+                    <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+                    <div className="modal-action">
+                        <a href="#" className="btn">Yay!</a>
+                    </div>
+                </div>
             </div>
         </div>
     );
--- src/page/MyToy/MyToyRow.jsx
+++ src/page/MyToy/MyToyRow.jsx
@@ -1,25 +1,32 @@
 import React from 'react';
 import { RiDeleteBin6Line, RiEdit2Line } from 'react-icons/ri';
 
-const MyToyRow = ({ toys,index }) => {
-    const { ToyName, price, quantity } = toys
-    console.log(toys);
-    return (
-        <tr>
-            <td>{index+1}</td>
-            <td className='shrink-1'>{ToyName}</td>
-            <td>{price}</td>
-            <td>{quantity}</td>
-            <td className='flex gap-2'>
-                <button className="flex items-center justify-center bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-2 rounded">
-                    <RiEdit2Line size={20} />
-                </button>
-                <button className="flex items-center justify-center bg-red-500 hover:bg-red-700 text-white font-semibold py-2 px-2 rounded">
-                    <RiDeleteBin6Line size={20} />
-                </button>
+const MyToyRow = ({ toys, index,handelOpenModal }) => {
+    const { _id,ToyName, price, quantity,description } = toys;
+    const toyInfo = {
+        ToyName, price, quantity,description,_id
+    }
 
-            </td>
-        </tr>
+    return (
+        <>
+            <tr>
+                <td>{index + 1}</td>
+                <td className='shrink-1'>{ToyName}</td>
+                <td>{price}</td>
+                <td>{quantity}</td>
+                <td className='flex gap-2'>
+
+                    <a onClick={()=>handelOpenModal(toyInfo)} href="#my-modal-2" className="flex items-center justify-center bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-2 rounded" ><RiEdit2Line size={20} /></a>
+
+
+                    <button className="flex items-center justify-center bg-red-500 hover:bg-red-700 text-white font-semibold py-2 px-2 rounded">
+                        <RiDeleteBin6Line size={20} />
+                    </button>
+
+                </td>
+            </tr>
+            
+        </>
     );
 };
 
